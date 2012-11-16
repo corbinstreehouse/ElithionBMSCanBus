a4f28ac5dcbcc895a740af1bb6653f69396af515
corbin dunn <corbind@apple.com>
2012-11-16 14:49:07 -0800

    more debug and a size fix

--- Canbus.cpp
+++ Canbus.cpp
@@ -26,6 +26,8 @@
 
 #include <HardwareSerial.h>
 
+#define DEBUG 1
+
 
 // http://lithiumate.elithion.com/php/menu_setup.php#Standard_output_messages
 // TODO: this are all configurable and would be nice to set for the class
@@ -67,22 +69,60 @@ bool CanbusClass::init(CanSpeed canSpeed) {
 #define PID_LO_OFFSET 3
 
 
-static bool sendAndReceiveMessage(tCAN *message, uint8_t pid_reply, uint8_t response_mode, uint8_t response_pid_hi, uint8_t response_pid_low) {
+static bool sendAndReceiveMessage(tCAN *message, uint16_t pid_reply, uint8_t response_mode, uint8_t response_pid_hi, uint8_t response_pid_low) {
 	mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
 	if (mcp2515_send_message(message)) {
+        Serial.println("message sent");
         // Timeout should be smarter than a loop!
         int timeout = 0;
         while (timeout < 4000) {
             timeout++;
             if (mcp2515_check_message()) {
+                Serial.println("message available");
                 if (mcp2515_get_message(message)) {
                     // See if we got the right response; making sure we got enough bytes (at least 3 to read the high and low
                     if ((message->id == pid_reply) && (message->data[NUM_BYTES_OFFSET] >= 3) && (message->data[MODE_OFFSET] == response_mode) && (message->data[PID_HI_OFFSET] == response_pid_hi) && (message->data[PID_LO_OFFSET] == response_pid_low)) {
                         return true;
+                    } else {
+#if DEBUG
+                        Serial.print("reply id: 0x");
+                        Serial.print(message->id, HEX);
+                        Serial.print("expecting id: 0x");
+                        Serial.print(pid_reply, HEX);
+                        Serial.print(" bytes: ");
+                        Serial.print(message->data[NUM_BYTES_OFFSET], HEX);
+                        Serial.print(" mode: ");
+                        Serial.print(message->data[MODE_OFFSET], HEX);
+                        Serial.print("expecting mode: 0x");
+                        Serial.print(response_mode, HEX);
+                        
+                        Serial.print(" pid hi: ");
+                        Serial.print(message->data[PID_HI_OFFSET], HEX);
+                        
+                        Serial.print("expecting hi: 0x");
+                        Serial.print(response_pid_hi, HEX);
+                        
+                        
+                        Serial.print(" pid low: ");
+                        Serial.print(message->data[PID_LO_OFFSET], HEX);
+
+                        Serial.print("expecting low: 0x");
+                        Serial.print(response_pid_low, HEX);
+                        
+                        Serial.println("");
+#endif
                     }
+                } else {
+#if DEBUG
+                    Serial.println("couldn't get the message even though one was available");
+#endif
                 }
             }
         }
+    } else {
+#if DEBUG
+        Serial.println("message NOT sent");
+#endif
     }
  	return false;
 }
@@ -104,11 +144,11 @@ static void setupElithionCanMessage(tCAN *message, uint8_t mode, uint8_t pid_hi,
 
 // readFromCanBusPID(ELITHION_PID_REQUEST, ELITHION_PID_MODE_PACK, ELITHION_PID_SOC, 0, ELITHION_PID_RESPONSE,
 
-static bool readElithionMessageFromCanBus(tCAN *message, uint8_t mode, uint8_t pid_hi, uint8_t pid_low, uint16_t pid_response_mode, uint16_t pid_response_hi, uint16_t pid_response_low)
-{
-    setupElithionCanMessage(message, mode, pid_hi, pid_low);
-    return sendAndReceiveMessage(message, ELITHION_PID_RESPONSE, pid_response_mode, pid_response_hi, pid_response_low);
-}
+//static bool readElithionMessageFromCanBus(tCAN *message, uint8_t mode, uint8_t pid_hi, uint8_t pid_low, uint16_t pid_response_mode, uint16_t pid_response_hi, uint16_t pid_response_low)
+//{
+//    setupElithionCanMessage(message, mode, pid_hi, pid_low);
+//    return sendAndReceiveMessage(message, ELITHION_PID_RESPONSE, pid_response_mode, pid_response_hi, pid_response_low);
+//}
 
 static bool readElithionDefaultMessageFromCanBus(tCAN *message, uint8_t pid_hi, uint8_t pid_low, uint16_t pid_response_hi, uint16_t pid_response_low)
 {
@@ -120,6 +160,7 @@ static bool readElithionDefaultMessageFromCanBus(tCAN *message, uint8_t pid_hi, 
 uint8_t CanbusClass::stateOfCharge() {
 	tCAN message;
     if (readElithionDefaultMessageFromCanBus(&message, ELITHION_PID_PACK_SOC, 0/*pid_low*/, 0x50/*response pid hi*/, 0/*response pid low*/)) {
+        Serial.println("got data!");
         return message.data[4]; // percentage 
     } else {
 #if DEBUG
